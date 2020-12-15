7d604da748e0b3984442fc20a8be3f9fb9770ecc
nashwalters <72932477+nashwalters@users.noreply.github.com>
2020-12-15 17:33:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 $(document).ready(function(){
-    $("#videos").empty();
+
     //event listener for yt button
     $("#youtube").click(function(e){
     e.preventDefault();
+    $("#videos").empty();  // clears videos when submit button clicked
     
     var key = "AIzaSyAa1zc7O33vu-6VA17JJFLnWPC9ckiXcOw";
     var search = $("#search").val();
     var maxResults= 6;
-    var video ="";
-    var ytUrl = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search;
-
+    var ytUrl = "https://www.googleapis.com/youtube/v3/search?key=" + key + "&videoEmbeddable=true&type=video&part=snippet&maxResults=" + maxResults + "&q=" + search;
+    
+        //creating ajax call for when the submit button is clicked.
         $.ajax({
             url:ytUrl,
             method: "GET"
         }).then(function(data) {
-         // console.log(data)
-
-         data.items.forEach(items => {
-            video =`<iframe width="420" height="315" src="https://www.youtube.com/embed/${items.id.videoId}" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>`
-
-            $("#videos").append(video)
-         });
-
+        
+        // for each loop for the data recieved.
+        $.each(data.items, function(i,item) {
+            //created p tag for video title.
+            var p = $("<p>");
+            p.text(item.snippet.title)
+            //append p tag and iframe with video id to video section.
+            $("#videos").append(p, 
+            `<iframe width="420" height="315" src="https://www.youtube.com/embed/${item.id.videoId}" frameborder="0" allow="accelerometer; encrypted-media" allowfullscreen></iframe>`);
+            });
         })
     })
 })
